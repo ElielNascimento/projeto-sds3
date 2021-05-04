3e7853e38b4f9aed596ce8b58f7c30825192a781
ElielNascimento <nascimentob.eliel@gmail.com>
2021-05-04 14:10:30 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
+
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+import eliel from 'components/NavBar';
+import React from 'react';
+
+
 function App() {
- return (
-  <div>
-    <h1 className="text-primary"> Ola mundo eliel! </h1>
-  
-  </div>
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary"> DsVendas </h1>
+        <DataTable />
+      </div>
+      <Footer />
+
+    </>
+
   );
 }
 export default App;
