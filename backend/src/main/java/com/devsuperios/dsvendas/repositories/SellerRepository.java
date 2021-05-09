d44547c96f5dbd0e55464d0a3730b17b149e1a92
ElielNascimento <nascimentob.eliel@gmail.com>
2021-05-09 11:29:22 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperios/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,20 @@
+package com.devsuperios.dsvendas.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperios.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+	List<Seller> findAll();
+
+	;
+	
+	
+	
+	
+	
+
+}
